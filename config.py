d056229694c448387c5120bc294e3562885cf09f
Dmitry Maevsky <mart@MacBook-Pro-Dmitry.local>
2020-02-11 11:31:20 +0300

    small int fix

--- config.py
+++ config.py
@@ -33,7 +33,6 @@ class Config(object):
         self._config[name] = value
 
     def get_property(self, property_name: str):
-        log(property_name)
         if property_name not in self._config.keys(): # we don't want KeyError
             return None  # just return None if not found
         return self._config[property_name]
